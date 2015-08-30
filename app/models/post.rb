8bb4fa2109dd494d93d64470464a24a8000bf921
Priscilla Rodriguez <priss@prisskreative.com>
2015-08-30 03:14:49 -0400

    Create routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+	has_many :comments
+
+
+    # let's also override the as_json method so that all json representations of our posts will include the comments.
+	def as_json(options = {})
+    super(options.merge(include: :comments))
+  end
+
 end
+
+
+## Using as_json is a simple way to change the json output of our models throughout our application, 
+## but can get complex over time as you your app grows. If you want to separate your view logic out 
+## of your models completely, you can look into a using a templating engine for json such as jbuilder 
+## which ships with Rails 4 by default or rabl-rails.
